afe5e74f87f3125d6e51f610693ad48d8a94e7df
Novis Shrivastava <novis@Amritas-MacBook-Air.local>
2019-03-23 20:28:22 +0530

    second commit

---
+++ project/pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about-page">
+        <h1>This is my fantastic "About Page"</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error provident beatae sint tenetur! Itaque veniam officia aut aspernatur qui inventore ut, dolores vel doloremque assumenda, corrupti beatae debitis asperiores rem?Vel, voluptas dolor mollitia facilis asperiores totam enim debitis iusto, alias harum ad aliquid eaque, at sint omnis? Ducimus commodi, porro modi dolorem aut alias. Sunt pariatur corrupti voluptatibus asperiores.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error provident beatae sint tenetur! Itaque veniam officia aut aspernatur qui inventore ut, dolores vel doloremque assumenda, corrupti beatae debitis asperiores rem?Vel, voluptas dolor mollitia facilis asperiores totam enim debitis iusto, alias harum ad aliquid eaque, at sint omnis? Ducimus commodi, porro modi dolorem aut alias. Sunt pariatur corrupti voluptatibus asperiores.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error provident beatae sint tenetur! Itaque veniam officia aut aspernatur qui inventore ut, dolores vel doloremque assumenda, corrupti beatae debitis asperiores rem?Vel, voluptas dolor mollitia facilis asperiores totam enim debitis iusto, alias harum ad aliquid eaque, at sint omnis? Ducimus commodi, porro modi dolorem aut alias. Sunt pariatur corrupti voluptatibus asperiores.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error provident beatae sint tenetur! Itaque veniam officia aut aspernatur qui inventore ut, dolores vel doloremque assumenda, corrupti beatae debitis asperiores rem?Vel, voluptas dolor mollitia facilis asperiores totam enim debitis iusto, alias harum ad aliquid eaque, at sint omnis? Ducimus commodi, porro modi dolorem aut alias. Sunt pariatur corrupti voluptatibus asperiores.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error provident beatae sint tenetur! Itaque veniam officia aut aspernatur qui inventore ut, dolores vel doloremque assumenda, corrupti beatae debitis asperiores rem?Vel, voluptas dolor mollitia facilis asperiores totam enim debitis iusto, alias harum ad aliquid eaque, at sint omnis? Ducimus commodi, porro modi dolorem aut alias. Sunt pariatur corrupti voluptatibus asperiores.</p>        
+    </div>
+</template>
+
+
+<style scoped>
+    .about-page{
+        text-align: center;
+        padding: 30px;
+    }
+</style>
